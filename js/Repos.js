0093122d11e6308575155d99627f8b0d173fba8a
Ian Baum <ian@ianbaum.com>
2016-10-14 10:13:24 -0400

    create sass styling and add repo info to webpage display

--- js/Repos.js
+++ js/Repos.js
@@ -6,6 +6,19 @@ function Repos(){
 Repos.prototype.getRepos = function(username){
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+    $(".selectedUserName").text(response.login);
+    $(".selectedUserAvatar").attr('src', response.avatar_url);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+    $(".selectedUserName").text("User not Found");
+  });
+
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    $(".selectedUserRepos").text("");
+    for(var i=0; i<response.length;i++){
+      $(".selectedUserRepos").append("<li class='repoName'>" + response[i].name + "<ul><li class='repoDate'>" + response[i].created_at +  "<li class='repoDesc'>" + response[i].description + "</li></ul>" + "</li>")
+    }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
